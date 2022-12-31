bd3a29c3c14e713de41a980aa3032350836846fa
Ahmet Erdi Hacıkahyaoğlu <ahmet-erdi@hotmail.com>
2022-12-31 12:02:41 +0300

    Initial commit

---
+++ src/main/java/com/garanti/FirstSpringWeb/model/Person.java
@@ -0,0 +1,13 @@
+package com.garanti.FirstSpringWeb.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private int yas;
+    private String isim;
+}
